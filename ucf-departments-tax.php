00ffd2bf7ed7bdd89516b2cb88d92ffc10f41fac
Jim Barnes <james.barnes@ucf.edu>
2017-06-12 16:23:02 -0400

    Added term meta for departments.

--- ucf-departments-tax.php
+++ ucf-departments-tax.php
@@ -11,4 +11,18 @@ if ( ! defined( 'WPINC' ) ) {
 
 define( 'UCF_DEPARTMENTS__FILE', __FILE__ );
 
+include_once 'includes/ucf-departments-taxonomy.php';
+include_once 'includes/ucf-departments-fields.php';
 
+// Run base actions inside of 'plugins_loaded' hook so we 
+// can check for the existence of other post_types and taxonomies
+if ( ! function_exists( 'ucf_departments_init' ) ) {
+	function ucf_departments_init() {
+		// Resgiter custom taxonomy
+		add_action( 'init', array( 'UCF_Departments_Taxonomy', 'register' ), 10, 0 );
+		// Register custom meta fields
+		add_action( 'init', array( 'UCF_Departments_Fields', 'register_meta_fields' ), 10, 0 );
+	}
+
+	add_action( 'plugins_loaded', 'ucf_departments_init' );
+}
