a3dd647473c6965a082cebd00e79dd396fbdb9c6
Jim Barnes <james.barnes@ucf.edu>
2017-06-12 16:26:14 -0400

    Added activation and deactivation hooks.

--- ucf-departments-tax.php
+++ ucf-departments-tax.php
@@ -14,7 +14,23 @@ define( 'UCF_DEPARTMENTS__FILE', __FILE__ );
 include_once 'includes/ucf-departments-taxonomy.php';
 include_once 'includes/ucf-departments-fields.php';
 
-// Run base actions inside of 'plugins_loaded' hook so we 
+if ( ! function_exists( 'ucf_departments_activation' ) ) {
+	function ucf_departments_activation() {
+		flush_rewrite_rules( true );
+	}
+
+	register_activation_hook( 'ucf_departments_activation', UCF_DEPARTMENTS__FILE );
+}
+
+if ( ! function_exists( 'ucf_departments_deactivation' ) ) {
+	function ucf_departments_deactivation() {
+		flush_rewrite_rules( true );
+	}
+
+	register_deactivation_hook( 'ucf_departments_deactivation', UCF_DEPARTMENTS__FILE );
+}
+
+// Run base actions inside of 'plugins_loaded' hook so we
 // can check for the existence of other post_types and taxonomies
 if ( ! function_exists( 'ucf_departments_init' ) ) {
 	function ucf_departments_init() {
